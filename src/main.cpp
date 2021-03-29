b35f54b3af39ba36c5166a1e479aab65ae1fecfa
David Oh <david@Davids-MacBook-Pro.local>
2021-03-29 21:29:14 +0200

    Hello Vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
-int main (int args, char** argv)
-{
-    std::cout << "Hello CMake!" << std::endl;
+int main () {
+
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(400, 400, "Hello Vulkan", nullptr, nullptr);
+
+    uint32_t extentionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+    std::cout << extentionCount << " extentions supported.\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vector;
+    auto test = matrix * vector;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
